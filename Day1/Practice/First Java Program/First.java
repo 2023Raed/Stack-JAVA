444d3c4c2016010f1f9f81cf9becc47149276f9b
2023Raed <raed.messaoud1993@gmail.com>
2023-04-11 16:43:32 +0100

    FirstjavaProgram

---
+++ Day1/Practice/First Java Program/First.java
@@ -0,0 +1,7 @@
+public class First {
+	public static void main(String[] args) {
+		System.out.println("My name is coding Dojo");
+        System.out.println("i am 100 years old");
+        System.out.println("My hometown is burbanks");
+	}
+}
