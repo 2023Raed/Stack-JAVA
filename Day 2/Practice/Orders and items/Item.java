ef0a2a32a8ef103fc06da193d013af32dc93775c
2023Raed <raed.messaoud1993@gmail.com>
2023-04-16 15:12:01 +0100

    Orders and items

---
+++ Day 2/Practice/Orders and items/Item.java
@@ -0,0 +1,10 @@
+public class Item{
+    // member variables
+     public String name;
+     public double price;
+// constructor
+public Item(String name, double price){
+this.name=name;
+this.price=price;
+}
+}
---
+++ Day 2/Practice/Orders and items/Order.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Order {
+    // member variables
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Item> items;
+    // constructor
+    public Order(String name) {
+        this.name=name;
+        this.total=total;
+        this.ready= ready;
+        this.items=new ArrayList<Item>();
+
+}
+public void addItem(Item item) {
+    this.items.add( item);
+}
+}
